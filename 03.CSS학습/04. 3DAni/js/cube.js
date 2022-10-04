4331231dc1ac02170a46a3e20181d40ac1012f79
LESEHY <tmdgus2496@gmail.com>
2022-10-04 16:00:21 +0900

    ss

---
+++ 03.CSS학습/04. 3DAni/js/cube.js
@@ -0,0 +1,8 @@
+// 큐브애니 JS - cube.js
+
+//---------------로드구역---------------//
+window.addEventListener("DOMContentLoaded",function(){
+    // 호출확인
+    console.log("로딩완료!");
+});
+//---------------로드구역---------------//
