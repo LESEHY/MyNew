a3951d2e6b5327d4141071ba199c9f5eb835bce6
LESEHY <tmdgus2496@gmail.com>
2022-10-04 16:07:43 +0900

    ss

--- 03.CSS학습/04. 3DAni/js/cube.js
+++ 03.CSS학습/04. 3DAni/js/cube.js
@@ -4,5 +4,20 @@
 window.addEventListener("DOMContentLoaded",function(){
     // 호출확인
     console.log("로딩완료!");
+
+    // 클릭이벤트 대상: .btns button
+    const btns = 
+    document.querySelector('.btns button');
+
+    // 클릭시 기능구현함수
+    const aniFn = function(){
+
+        // 1. 호출확인
+        console.log("나야나!")
+    };
+
+    // 대상에 클릭설정하기
+    btns.onclick =aniFn;
+
 });
 //---------------로드구역---------------//
