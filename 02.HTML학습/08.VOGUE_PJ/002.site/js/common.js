f50d14516915d9165253e4b5b8e9d031d8df8809
LESEHY <tmdgus2496@gmail.com>
2022-10-31 14:20:13 +0900

    ss

---
+++ 02.HTML학습/08.VOGUE_PJ/002.site/js/common.js
@@ -0,0 +1,35 @@
+// 보그 PJ 공통JS - common.js
+
+// 제이쿼리 구역 길게쓰기도 있음
+// $(document).ready(function(){})
+
+$(() => {
+    console.log("로딩완료");
+
+    ////////// 스크롤 이벤트 구역 ///////
+    // 이벤트명 : scroll
+    // 이벤트 대상 : window
+    // 스크롤위치변수
+    let scTop;
+
+    $(window).scroll(() => {
+        // 스크롤 위치값 (this는 window)
+        scTop = $(this).scrollTop()
+        // scrollTop() 메서드 
+        // - 세로스크롤 위치값을 리턴하는 메서드
+        console.log(scTop);
+
+        // 1. 슬림메뉴 클래스 on적용!
+        // 기준위치는 스크롤위치 100px이상
+        if(scTop >= 100){ // 100px 이상
+            $().addClass("on");
+            // addClass(클래스명) - 클래스 넣기
+        } // if 
+
+        else{ // 100px 미만
+            $().removeClass("on");
+            // removeClass(클래스명) = 클래스 지우기
+        } // else
+    }); // scroll 
+
+}); //jQB
