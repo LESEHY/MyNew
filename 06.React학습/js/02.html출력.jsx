9f6d350494f72fe646ab0f1dd93a7abd9dcc3b0c
LESEHY <tmdgus2496@gmail.com>
2022-11-11 15:04:32 +0900

    ss

--- 06.React학습/js/02.html출력.jsx
+++ 06.React학습/js/02.html출력.jsx
@@ -2,29 +2,33 @@
 
 // 변수에 태그를 JSX문법으로 작성하여 할당한다!
 const myelement = (
+  <div>
+    <h1>♣ 나의 친구 리스트</h1>
     <table>
-        <tr>
-            <th>이름</th>
-            <th>전화번호</th>
-            <th>생일</th>
-        </tr>
-        <tr>
-            <td>전소민</td>
-            <td>010-2222-3333</td>
-            <td>90.03.04</td>
-        </tr>
-        <tr>
-            <td>김혁수</td>
-            <td>010-1234-5678</td>
-            <td>02.05.06</td>
-        </tr>
-        <tr>
-            <td>이상화</td>
-            <td>010-0000-1111</td>
-            <td>00.07.08</td>
-        </tr>
+      <tr>
+        <th>이름 </th>
+        <th>전화번호</th>
+        <th>생일</th>
+      </tr>
+      <tr>
+        <td>전소민</td>
+        <td>010-2222-3333</td>
+        <td>90.03.04</td>
+      </tr>
+      <tr>
+        <td>김혁수</td>
+        <td>010-1234-5678</td>
+        <td>02.05.06</td>
+      </tr>
+      <tr>
+        <td>이상화</td>
+        <td>010-0000-1111</td>
+        <td>00.07.08</td>
+      </tr>
     </table>
+  </div>
 );
 
 // 화면출력
-ReactDOM.render(myelement,document.getElementById(''));
+// ReactDOM.render(출력할요소, 출력요소)
+ReactDOM.render(myelement, document.getElementById("root"));
