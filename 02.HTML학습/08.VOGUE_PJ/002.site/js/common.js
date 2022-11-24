1fc25941b106596dd318241664a7f38c9c58247d
LESEHY <tmdgus2496@gmail.com>
2022-11-24 16:20:07 +0900

    ss

--- 02.HTML학습/08.VOGUE_PJ/002.site/js/common.js
+++ 02.HTML학습/08.VOGUE_PJ/002.site/js/common.js
@@ -1,15 +1,30 @@
 // 보그 PJ 공통JS - common.js
 
+// 윈도우 가로 크기가 모바일 사이즈면 코드를 변경한다!
+// 모바일일때 적용하고 싶거나 싶지않으면 mobsts값을 활용한다!
+// 모바일일때 1, 모바일이 아닐때가 0 (가로크기 500기준)
+let mobsts = 0;
+if ($(window).width() <= 500) mobsts = 1;
+console.log("모바일적용여부: ", mobsts);
+
+// 이건 순전히 개발자들 브라우저 크기변경 테스트때문에 구현함!
+// 브라우저 화면크기변경시 계속발생!
+$(window).resize(() => {
+    if ($(window).width() <= 500) mobsts = 1;
+    else mobsts = 0;
+    console.log("모바일적용여부(resize): ", mobsts);
+}); // resize
+
 // 현재 페이지명을 알아내어 제어에 활용한다!
 // 페이지명 변수
 let pname = location.pathname;
 // location.pathname 페이지명이 포함된 전체경로
 // split(자를 문자열) -> 배열에 담긴다!
 pname = pname.split("/"); //배열에 담는다
-pname = pname[pname.length-1]; // 마지막 배열
+pname = pname[pname.length - 1]; // 마지막 배열
 pname = pname.split(".")[0]; // 페이지 이름만 가져옴
-console.log("페이지이름: ", pname);
-console.log(pname);
+// console.log("페이지이름: ", pname);
+// console.log(pname);
 
 
 
@@ -17,7 +32,7 @@ console.log(pname);
 // $(document).ready(function(){})
 
 $(() => {
-    console.log("로딩완료");
+    // console.log("로딩완료");
 
     // 부드러운 스크롤 호출(제이쿼리 아님)
     startSS();
@@ -65,20 +80,20 @@ $(() => {
     }); // each 메서드
 
     // 위치배열값 확인하기
-    console.log("위치배열값 : ", scpos);
+    // console.log("위치배열값 : ", scpos);
 
     //////////////////////////////////////
     ///////// 스크롤 이벤트 함수 //////////
     $(window).scroll(() => {
         // 슬림메뉴와 상단이동버튼 보이기 작동안할 페이지 셋팅
-        if(
-            pname === "login"||
-            pname === "member"||
+        if (
+            pname === "login" ||
+            pname === "member" ||
             pname === "gallery"
-        ){
+        ) {
             return; // 여기서 나감
         } // if
-    
+
         // 스크롤 위치값 (this는 window)
         scTop = $(this).scrollTop()
         // scrollTop() 메서드 
@@ -87,7 +102,8 @@ $(() => {
 
         // 1. 슬림메뉴 클래스 on적용!
         // 기준위치는 스크롤위치 100px이상
-        if (scTop >= 100) { // 100px 이상
+        // mobsts === 0 모바일이 아닐 때만 들어옴!
+        if (scTop >= 100 && mobsts === 0) { // 100px 이상
             topA.addClass("on");
             // addClass(클래스명) - 클래스 넣기
 
@@ -137,7 +153,7 @@ $(() => {
         // 3. 등장액션 적용하기
         // 스크롤 등장액션 검사함수 호출!
         // 등장요소 개수만큼 자동으로 돌아주며 호출!
-        scAct.each(idx=>scAction(idx));
+        scAct.each(idx => scAction(idx));
 
 
     }); // scroll 
